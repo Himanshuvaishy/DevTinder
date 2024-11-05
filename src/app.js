a29504c6e853ab75bb4988f25d5748908c016d52
Himanshu <vaishhimanshu83170@gmail.com>
2024-11-05 10:23:44 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-const express=require("express");
+const express = require("express");
+const app = express();
 
-const app=express();
 
-app.use("/hello",(req,res)=>{
-    //console.log("hellow nodejs");
+app.get("/user/:id/:firstName/:lastName",(req,res)=>{
+    //fetching data
+    res.send({firstName:"Himanshu",lastName:"vaishy"});
 
-    res.send("hello nodejs")
+      console.log(req.query);// print query 
+    console.log(req.params);// handle dyanamic route 
+    
     
 })
 
-// app.use("/test",(req,res)=>{
-//     res.send("I am test my api");
 
-// })
 
-app.listen(7777,()=>{
-    console.log("app is listening");
-    
+
+app.post("/user",(req,res)=>{
+    //saving data 
+    res.send("data is saved to database");
 })
+
+app.listen(7777, () => {
+  console.log("app is listening");
+});
